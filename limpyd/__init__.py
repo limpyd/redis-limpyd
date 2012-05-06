6f4151c5d95f0e4faef092879a47451948ef0c39
twidi <s.angel@twidi.com>
2012-05-06 23:29:15 +0200

    First version of setup.py

--- limpyd/__init__.py
+++ limpyd/__init__.py
@@ -1,3 +1,12 @@
+"Idea is to provide an *easy* way to store python objects in Redis, *without losing the power and the control of the Redis API*, in a ''limpid'' way. So, scope is to provide just as abstraction as needed."
+VERSION = (0, 0, 1)
+
+__author__ = 'Yohan Bonifacel'
+__contact__ = "y.boniface@liberation.fr"
+__homepage__ = "https://github.com/liberation/redis-limpyd"
+__version__ = ".".join(map(str, VERSION))
+
+
 import redis
 
 # TODO Make it customisable
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+
+from setuptools import setup, find_packages
+
+import limpyd
+
+long_description = codecs.open('README.rst', "r", "utf-8").read()
+
+setup(
+    name = "redis-limpyd",
+    version = limpyd.__version__,
+    author = limpyd.__author__,
+    author_email = limpyd.__contact__,
+    description = limpyd.__doc__,
+    keywords = "redis",
+    url = limpyd.__homepage__,
+    download_url = "https://github.com/liberation/redis-limpyd/downloads",
+    packages = find_packages(),
+    include_package_data=True,
+    #license = "MIT",
+    platforms=["any"],
+    zip_safe=True,
+
+    long_description = long_description,
+
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        #"Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        #"License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+    ],
+)
+
