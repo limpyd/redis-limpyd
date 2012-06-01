3ae44152817ad7576eddf0c3a45c589d04bb4deb
Yohan Boniface <y.boniface@liberation.fr>
2012-06-01 10:29:02 +0200

    list() sounds enough here

--- limpyd/model.py
+++ limpyd/model.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 
 from logging import getLogger
-from copy import copy
 
 from limpyd import get_connection
 from limpyd.fields import *
@@ -20,8 +19,8 @@ class MetaRedisModel(MetaRedisProxy):
     def __new__(mcs, name, base, dct):
         it = type.__new__(mcs, name, base, dct)
         # We make invisible for user that fields where class properties
-        _fields = copy(it._fields) if hasattr(it, '_fields') else []
-        _hashable_fields = copy(it._hashable_fields) if hasattr(it, '_hashable_fields') else []
+        _fields = list(it._fields) if hasattr(it, '_fields') else []
+        _hashable_fields = list(it._hashable_fields) if hasattr(it, '_hashable_fields') else []
         attrs = dir(it)
         for attr_name in attrs:
             if attr_name.startswith("_"):
