9460c68b95554a78796b74d839edc88aad05881a
twidi <s.angel@twidi.com>
2012-08-05 00:28:53 +0200

    Add tests for FK related fields

--- tests/contrib/related.py
+++ tests/contrib/related.py
@@ -42,7 +42,76 @@ class Group(TestRedisModel):
 
 
 class FKTest(LimpydBaseTest):
-    pass
+
+    def test_can_access_reverse_fk(self):
+        core_devs = Group(name='limpyd core devs')
+        ybon = Person(name='ybon')
+
+        core_devs.owner.hset(ybon._pk)
+        self.assertEqual(set(Group.collection(owner=ybon._pk)), set([core_devs._pk]))
+        self.assertEqual(set(ybon.owned_groups()), set([core_devs._pk]))
+
+    def test_fk_can_be_given_as_object(self):
+        core_devs = Group(name='limpyd core devs')
+        ybon = Person(name='ybon')
+
+        core_devs.owner.hset(ybon)
+        self.assertEqual(core_devs.owner.hget(), ybon._pk)
+        self.assertEqual(set(ybon.owned_groups()), set([core_devs._pk]))
+
+    def test_can_update_fk(self):
+        core_devs = Group(name='limpyd core devs')
+        ybon = Person(name='ybon')
+        twidi = Person(name='twidi')
+
+        core_devs.owner.hset(ybon)
+        self.assertEqual(set(ybon.owned_groups()), set([core_devs._pk]))
+
+        core_devs.owner.hset(twidi)
+        self.assertEqual(set(ybon.owned_groups()), set())
+        self.assertEqual(set(twidi.owned_groups()), set([core_devs._pk]))
+
+    def test_many_fk_can_be_set_on_same_object(self):
+        core_devs = Group(name='limpyd core devs')
+        fan_boys = Group(name='limpyd fan boys')
+        twidi = Person(name='twidi')
+
+        core_devs.owner.hset(twidi)
+        fan_boys.owner.hset(twidi)
+        self.assertEqual(set(twidi.owned_groups()), set([core_devs._pk, fan_boys._pk]))
+
+    def test_fk_can_be_set_on_same_model(self):
+        main_group = Group(name='limpyd groups')
+        core_devs = Group(name='limpyd core devs')
+        fan_boys = Group(name='limpyd fan boys')
+
+        core_devs.parent.set(main_group)
+        fan_boys.parent.set(main_group)
+        self.assertEqual(set(main_group.children()), set([core_devs._pk, fan_boys._pk]))
+
+    def test_deleting_an_object_must_clear_the_fk(self):
+        main_group = Group(name='limpyd groups')
+        core_devs = Group(name='limpyd core devs')
+        fan_boys = Group(name='limpyd fan boys')
+        ybon = Person(name='ybon')
+
+        core_devs.owner.hset(ybon)
+        ybon.delete()
+        self.assertIsNone(core_devs.owner.hget())
+
+        core_devs.parent.set(main_group)
+        fan_boys.parent.set(main_group)
+        main_group.delete()
+        self.assertIsNone(core_devs.parent.get())
+        self.assertIsNone(fan_boys.parent.get())
+
+    def test_deleting_a_fk_must_clean_the_collection(self):
+        core_devs = Group(name='limpyd core devs')
+        ybon = Person(name='ybon')
+
+        core_devs.owner.hset(ybon)
+        core_devs.delete()
+        self.assertEqual(set(ybon.owned_groups()), set())
 
 
 class M2MSetTest(LimpydBaseTest):
