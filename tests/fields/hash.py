d0053ca0c907cde13d2ab59b3d9130fbc5ff2851
Yohan Boniface <yb@enix.org>
2013-01-09 22:01:13 +0100

    Use assertCollection in tests/fields/hash.py

--- tests/fields/hash.py
+++ tests/fields/hash.py
@@ -2,18 +2,20 @@
 
 from limpyd import fields
 
-from ..base import LimpydBaseTest
-from ..model import TestRedisModel
+from ..model import TestRedisModel, BaseModelTest
 
 
-class HashFieldTest(LimpydBaseTest):
+class EmailTestModel(TestRedisModel):
+    headers = fields.HashField(indexable=True)
+    raw_headers = fields.HashField()
 
-    class EmailTestModel(TestRedisModel):
-        headers = fields.HashField(indexable=True)
-        raw_headers = fields.HashField()
+
+class HashFieldTest(BaseModelTest):
+
+    model = EmailTestModel
 
     def test_hmset_should_set_values(self):
-        obj = self.EmailTestModel()
+        obj = self.model()
         headers = {
             'from': 'foo@bar.com',
             'to': 'me@world.org'
@@ -23,40 +25,40 @@ class HashFieldTest(LimpydBaseTest):
         self.assertEqual(obj.headers.hget('to'), 'me@world.org')
 
     def test_hmset_should_be_indexable(self):
-        obj = self.EmailTestModel()
+        obj = self.model()
         obj.headers.hmset(**{'from': 'you@moon.io'})
-        self.assertEqual(set(self.EmailTestModel.collection(headers__from='you@moon.io')), set([obj._pk]))
+        self.assertEqual(set(self.model.collection(headers__from='you@moon.io')), set([obj._pk]))
 
         # Now change value and check first has been deindexed and new redindexed
         obj.headers.hmset(**{'from': 'you@mars.io'})
-        self.assertEqual(set(self.EmailTestModel.collection(headers__from='you@moon.io')), set())
-        self.assertEqual(set(self.EmailTestModel.collection(headers__from='you@mars.io')), set([obj._pk]))
+        self.assertCollection([], headers__from='you@moon.io')
+        self.assertCollection([obj._pk], headers__from='you@mars.io')
 
     def test_hset_should_set_value_and_be_indexable(self):
-        obj = self.EmailTestModel()
+        obj = self.model()
         obj.headers.hset('from', 'someone@cassini.io')
         self.assertEqual(obj.headers.hget('from'), 'someone@cassini.io')
 
-        self.assertEqual(set(self.EmailTestModel.collection(headers__from='someone@cassini.io')), set([obj._pk]))
+        self.assertEqual(set(self.model.collection(headers__from='someone@cassini.io')), set([obj._pk]))
 
         # Now change value and check first has been deindexed and new redindexed
         obj.headers.hset('from', 'someoneelse@cassini.io')
-        self.assertEqual(set(self.EmailTestModel.collection(headers__from='someone@cassini.io')), set())
-        self.assertEqual(set(self.EmailTestModel.collection(headers__from='someoneelse@cassini.io')), set([obj._pk]))
+        self.assertCollection([], headers__from='someone@cassini.io')
+        self.assertCollection([obj._pk], headers__from='someoneelse@cassini.io')
 
     def test_hincrby_should_set_value_and_be_indexable(self):
-        obj = self.EmailTestModel()
+        obj = self.model()
         obj.headers.hincrby('Message-ID', 1)
         self.assertEqual(obj.headers.hget('Message-ID'), '1')
-        self.assertEqual(set(self.EmailTestModel.collection(**{'headers__Message-ID': '1'})), set([obj._pk]))
+        self.assertCollection([obj._pk], **{'headers__Message-ID': '1'})
         # Now change value and check first has been deindexed and new redindexed
         obj.headers.hincrby('Message-ID', 1)
         self.assertEqual(obj.headers.hget('Message-ID'), '2')
-        self.assertEqual(set(self.EmailTestModel.collection(**{'headers__Message-ID': '1'})), set())
-        self.assertEqual(set(self.EmailTestModel.collection(**{'headers__Message-ID': '2'})), set([obj._pk]))
+        self.assertCollection([], **{'headers__Message-ID': '1'})
+        self.assertCollection([obj._pk], **{'headers__Message-ID': '2'})
 
     def test_delete_hashfield(self):
-        obj = self.EmailTestModel()
+        obj = self.model()
         headers = {
             'from': 'foo@bar.com',
             'to': 'me@world.org'
@@ -66,14 +68,14 @@ class HashFieldTest(LimpydBaseTest):
         self.assertEqual(obj.headers.hget('to'), 'me@world.org')
         obj.headers.hdel('from')
         self.assertEqual(obj.headers.hget('from'), None)
-        self.assertEqual(set(self.EmailTestModel.collection(headers__from='foo@bar.com')), set())
+        self.assertCollection([], headers__from='foo@bar.com')
 
         # Do not raise if we try to del a key that does not exist
         # (follow redis usage)
         obj.headers.hdel('a key that does not exist')
 
     def test_hsetnx_should_index_only_if_value_is_new(self):
-        obj = self.EmailTestModel()
+        obj = self.model()
         obj.headers.hset('to', 'two@three.org')
         with self.assertNumCommands(5):
             # three calls for lock
@@ -94,7 +96,7 @@ class HashFieldTest(LimpydBaseTest):
         self.assertEqual(obj.headers.hget('from'), 'one@two.org')
 
     def test_hgetall_should_return_a_dict(self):
-        obj = self.EmailTestModel()
+        obj = self.model()
         headers = {
             'from': 'foo@bar.com',
             'to': 'me@world.org'
