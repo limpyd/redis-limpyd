e4792ecd815cb2fe7e26f9e9764c8ccffe69ae9b
Yohan Boniface <yb@enix.org>
2013-02-12 01:45:27 +0100

    Bump version to 0.1.0

--- limpyd/__init__.py
+++ limpyd/__init__.py
@@ -1,5 +1,8 @@
-"Idea is to provide an *easy* way to store python objects in Redis, *without losing the power and the control of the Redis API*, in a ''limpid'' way. So, scope is to provide just as abstraction as needed."
-VERSION = (0, 0, 1)
+"""Limpyd provides an easy way to store objects in Redis, without losing the
+power and the control of the Redis API, in a limpid way, with just as
+abstraction as needed."""
+
+VERSION = (0, 1, 0)
 
 __author__ = 'Yohan Boniface'
 __contact__ = "yb@enix.org"
