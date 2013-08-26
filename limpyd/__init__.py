ef65851c42b11b498a3829b93378565467542f5d
twidi <s.angel@twidi.com>
2013-08-26 16:08:58 +0200

    Include only the "limpyd" package in setup.py (no tests)

--- limpyd/__init__.py
+++ limpyd/__init__.py
@@ -2,7 +2,7 @@
 power and the control of the Redis API, in a limpid way, with just as
 abstraction as needed."""
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 
 __author__ = 'Yohan Boniface'
 __contact__ = "yb@enix.org"
